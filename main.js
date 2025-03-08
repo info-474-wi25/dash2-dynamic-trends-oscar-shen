d58d47e7b033cf15d1c45feecc3d535b2573ac39
oscar_shen <cshen5@uw.edu>
2025-03-07 16:53:42 -0800

    Finish

--- main.js
+++ main.js
@@ -61,6 +61,7 @@ d3.csv("aircraft_incidents.csv").then(data => {
         .call(d3.axisBottom(xScale1).tickFormat(d3.format("d")));
 
     svg1_IncidentsTrend.append("g")
+        .attr("class", "y-axis")
         .call(d3.axisLeft(yScale1));
 
     // 6.a: AXIS LABELS & TITLE
@@ -85,5 +86,55 @@ d3.csv("aircraft_incidents.csv").then(data => {
         .text("Number of Incidents");
 
     // 7.a: INTERACTIVITY - POINTS AND TOOLTIP
+    svg1_IncidentsTrend.selectAll("circle")
+        .data(incidentsData)
+        .join("circle")
+        .attr("cx", d => xScale1(d.year))
+        .attr("cy", d => yScale1(d.count))
+        .attr("r", 5)
+        .attr("fill", "orange")
+        .on("mouseover", (event, d) => {
+            tooltip.style("opacity", 1)
+                .html(`Year: ${d.year}<br>Incidents: ${d.count}`)
+                .style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 10) + "px");
+        })
+        .on("mouseout", () => tooltip.style("opacity", 0));
+
+    // Interactive widget
+    const yearDropdown = d3.select("#yearDropdown");
+
+    yearDropdown.selectAll("option")
+        .data(incidentsData.map(d => d.year))  // Extract only the years
+        .enter()
+        .append("option")
+        .attr("value", d => d)
+        .text(d => d);
+
+    function updateChartByDropdown(selectedYear) {
+        svg1_IncidentsTrend.selectAll(".highlighted-point").remove();
+
+        const selectedData = incidentsData.find(d => d.year == selectedYear);
+
+        if (selectedData) {
+            svg1_IncidentsTrend.append("circle")
+                .attr("class", "highlighted-point")
+                .attr("cx", xScale1(selectedData.year))
+                .attr("cy", yScale1(selectedData.count))
+                .attr("r", 8)
+                .attr("fill", "red")
+                .attr("stroke", "black")
+                .attr("stroke-width", 2);
+
+            tooltip.style("opacity", 1)
+                .html(`Year: ${selectedData.year}<br>Incidents: ${selectedData.count}`)
+                .style("left", `${xScale1(selectedData.year) + margin.left + 10}px`)
+                .style("top", `${yScale1(selectedData.count) + margin.top - 10}px`);
+        }
+    }
+
+    yearDropdown.on("change", function () {
+        updateChartByDropdown(this.value);
+    });
 
 });
